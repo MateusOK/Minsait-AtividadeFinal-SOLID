388647b0dafc880cb891d4573cd6c9f89bd14114
MateusOK <mateusribeiro-2004@hotmail.com>
2023-03-28 14:11:48 -0300

    Adding method 'verificar_saldo'

--- contaCorrente.py
+++ contaCorrente.py
@@ -5,12 +5,15 @@ class ContaCorrente(Conta):
         super().__init__(conta_id, saldo)
         self.limite = limite
 
-    def depositar(self, valor_a_ser_depositado):
+    def depositar(self, valor_a_ser_depositado: float):
         self._saldo += valor_a_ser_depositado
     
-    def sacar(self, valor_a_ser_sacado):
+    def sacar(self, valor_a_ser_sacado: float):
         if valor_a_ser_sacado > self._saldo + self.limite:
-            raise ValueError("Valor a ser sacado não pode ser maior que saldo e limite juntos")
+            raise ValueError("Valor a ser sacado não pode ser maior que saldo e limite somados") #Caso o valor a ser sacado seja maior que o limite e saldo juntos uma Exception do tipo ValueError é jogada
         else:
             self._saldo -= valor_a_ser_sacado
+    
+    def verificar_saldo(self):
+        print("Saldo atual: ", self._saldo)
 
--- contaPoupanca.py
+++ contaPoupanca.py
@@ -6,12 +6,14 @@ class ContaPoupanca(Conta):
         super().__init__(conta_id, saldo)
         self.taxa_de_rendimento = taxa_de_rendimento
 
-    def depositar(self, valor_a_ser_depositado):
+    def depositar(self, valor_a_ser_depositado: float):
         self._saldo += valor_a_ser_depositado
 
-    def sacar(self, valor_a_ser_sacado):
-        self._saldo - + valor_a_ser_sacado
-
+    def sacar(self, valor_a_ser_sacado: float):
+        self._saldo -+ valor_a_ser_sacado
+    
+    def verificar_saldo(self):
+        return self._saldo
 
     def verificar_rendimento_ao_ano(self, tempo_de_rendimento):
         unidades_de_tempo = {
@@ -25,16 +27,16 @@ class ContaPoupanca(Conta):
         }
         if len(tempo_de_rendimento) < 2: #Verifica se a unidade de tempo fornecida tenha pelo menos dois caracteres
             print("Unidade de tempo não suportada.")
-            
+
         tempo_de_rendimento = tempo_de_rendimento.lower() # Tranformamos a string tempo fornecida para minúsculas para não ocorrer problema caso o usuário forneca com maiúsculas
         
         unidade_de_tempo_escolhida = tempo_de_rendimento[-3:] if tempo_de_rendimento[-3:] == 'min' else tempo_de_rendimento[-1] #Verifica qual unidade de tempo fornecida, utiliza :-3 como padrão caso o usuário forneca o tempo em minutos, se o resultado nao for 'min', pegamos a ultima letra para usar como unidade de tempo
         if unidade_de_tempo_escolhida not in unidades_de_tempo:                                                                                           
             print("Unidade de tempo não suportada.")
         if unidade_de_tempo_escolhida == 'min': #Caso a unidade de tempo fornecida for em 'min', fazemos o casting da do tempo fornecido para int excluindo as ultimas 3 letras e dividimos unidade de tempo correspondente
-            tempo_fracionado = int(tempo_de_rendimento[:-3]) / unidades_de_tempo['min']
+            tempo_fracionado = float(tempo_de_rendimento[:-3]) / unidades_de_tempo['min']
         else:
-            tempo_fracionado = int(tempo_de_rendimento[:-1]) / unidades_de_tempo[unidade_de_tempo_escolhida] #Se a unidade de tempo não for 'min', excluímos a ultima letra da string tempo fornecida e divimos pela unidade de tempo correspondente
+            tempo_fracionado = float(tempo_de_rendimento[:-1]) / unidades_de_tempo[unidade_de_tempo_escolhida] #Se a unidade de tempo não for 'min', excluímos a ultima letra da string tempo fornecida e divimos pela unidade de tempo correspondente
 
         # Cálculo do saldo com rendimento
         return self._saldo * (1 + self.taxa_de_rendimento/100) ** tempo_fracionado
