9d5aed7b2c8042435f98f2e4024ff43eb6be403d
MateusOK <mateusribeiro-2004@hotmail.com>
2023-03-28 13:20:10 -0300

    fix: 'min' not a supprted time unit is now supported

--- contaPoupanca.py
+++ contaPoupanca.py
@@ -13,8 +13,8 @@ class ContaPoupanca(Conta):
         self._saldo - + valor_a_ser_sacado
 
 
-    def verificar_rendimento_ao_ano(self, tempo):
-        time_units = {
+    def verificar_rendimento_ao_ano(self, tempo_de_rendimento):
+        unidades_de_tempo = {
             's': 31536000,   # segundos em um ano
             'min': 525600,   # minutos em um ano
             'h': 8760,       # horas em um ano
@@ -23,12 +23,19 @@ class ContaPoupanca(Conta):
             'm': 12,         # meses em um ano
             'a': 1,          # anos
         }
-        time_unit = tempo[-1]
-        if time_unit not in time_units:
+        if len(tempo_de_rendimento) < 2: #Verifica se a unidade de tempo fornecida tenha pelo menos dois caracteres
             print("Unidade de tempo não suportada.")
-            return
-        time_frac = int(tempo[:-1]) / time_units[time_unit]
+            
+        tempo_de_rendimento = tempo_de_rendimento.lower() # Tranformamos a string tempo fornecida para minúsculas para não ocorrer problema caso o usuário forneca com maiúsculas
+        
+        unidade_de_tempo_escolhida = tempo_de_rendimento[-3:] if tempo_de_rendimento[-3:] == 'min' else tempo_de_rendimento[-1] #Verifica qual unidade de tempo fornecida, utiliza :-3 como padrão caso o usuário forneca o tempo em minutos, se o resultado nao for 'min', pegamos a ultima letra para usar como unidade de tempo
+        if unidade_de_tempo_escolhida not in unidades_de_tempo:                                                                                           
+            print("Unidade de tempo não suportada.")
+        if unidade_de_tempo_escolhida == 'min': #Caso a unidade de tempo fornecida for em 'min', fazemos o casting da do tempo fornecido para int excluindo as ultimas 3 letras e dividimos unidade de tempo correspondente
+            tempo_fracionado = int(tempo_de_rendimento[:-3]) / unidades_de_tempo['min']
+        else:
+            tempo_fracionado = int(tempo_de_rendimento[:-1]) / unidades_de_tempo[unidade_de_tempo_escolhida] #Se a unidade de tempo não for 'min', excluímos a ultima letra da string tempo fornecida e divimos pela unidade de tempo correspondente
 
         # Cálculo do saldo com rendimento
-        saldo_rendimento = self._saldo * (1 + self.taxa_de_rendimento/100) ** time_frac
-        return saldo_rendimento
+        return self._saldo * (1 + self.taxa_de_rendimento/100) ** tempo_fracionado
+    
